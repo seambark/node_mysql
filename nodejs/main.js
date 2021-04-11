1e90230ebe0fe9545fc1be9e275ba1a5526f17c7
seambark <run128@naver.com>
2021-04-11 12:21:57 +0900

    제목 및 파일을 이용한 본문구현

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,18 +1,45 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function (request, response) {
-    var url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    // var queryData = new URL(_url);
+    console.log(queryData.query);
+    var title = queryData.id;
+
+    if (_url == '/') {
+        title = 'Welcome';
     }
-    if (request.url == '/favicon.ico') {
+    if (_url == '/favicon.ico') {
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    // console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
 
+    fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        var template = `
+        <!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <div>${description}</div>
+            </body>
+        </html>
+        `;
+        response.end(template);
+    });
 });
-app.listen(8090);
+app.listen(8800);
